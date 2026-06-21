f7c9361d91f2f4c2e7138e0bdfb767a0aa6571b0
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-21 15:45:35 -0500

    fixed problems
    fixed issues that where caused

--- linked list.cpp
+++ linked list.cpp
@@ -7,7 +7,7 @@ class Linkedlist {
 
         struct node { // like a class just more public used fro small data//
             int elem;
-            node* next
+            node* next;
 
             node(int x){
                 elem = x;
@@ -41,7 +41,7 @@ class Linkedlist {
         node* n = new node(data);
         
         if(head != nullptr){
-            n.next = head; // or: n->next = head;//
+            n->next = head; // or: n->next = head;//
         }
         head = n;
     }
@@ -53,7 +53,7 @@ int main(){
 
     Linkedlist l;
     for(int i = 0; i < 20; i++){
-        l.pushfront(i);
+        l.push_front(i);
     }
 
     return 0;
