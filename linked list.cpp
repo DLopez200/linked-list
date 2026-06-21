97cc9c6eeab1d766e93b9f897fafc11c003e004d
DLopez200 <ld30930@mail.harpercollege.edu>
2026-06-21 15:42:54 -0500

    linked lists
    created a linked list class that add and destroys elements to a list

---
+++ linked list.cpp
@@ -0,0 +1,60 @@
+#include <iostream> 
+
+using namespace std;
+
+class Linkedlist {
+    private :
+
+        struct node { // like a class just more public used fro small data//
+            int elem;
+            node* next
+
+            node(int x){
+                elem = x;
+                next = nullptr;
+                cout << "Constructed a node with " << elem <<endl;
+            }
+
+            ~node(){
+                if(next != nullptr){
+                delete next;
+                }
+                cout <<"Detroyed a node with " <<elem <<endl;
+            }
+        };
+    
+    public:
+
+    node* head; //first node//
+
+    Linkedlist(){
+        head = nullptr;
+    }
+
+    ~Linkedlist(){
+        if(head != nullptr){
+                delete head;
+                }
+    }
+
+    void push_front(int data){
+        node* n = new node(data);
+        
+        if(head != nullptr){
+            n.next = head; // or: n->next = head;//
+        }
+        head = n;
+    }
+   
+
+};
+
+int main(){
+
+    Linkedlist l;
+    for(int i = 0; i < 20; i++){
+        l.pushfront(i);
+    }
+
+    return 0;
+}
